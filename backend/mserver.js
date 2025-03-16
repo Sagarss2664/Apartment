5ec705499b245165fbc4be1ceb088f40371603cf
SAGAR S SHEGUNASHI <130956680+Sagarss2664@users.noreply.github.com>
2025-03-16 22:27:33 +0530

    Update mserver.js

--- backend/mserver.js
+++ backend/mserver.js
@@ -1471,7 +1471,6 @@ app.listen(PORT, () => {
 });
 ///////////////////////////////////////////////////////////////////////////////////////////
 // //Email Message sending
-import nodemailer from 'nodemailer';
 
 import { MongoClient } from 'mongodb';
 
