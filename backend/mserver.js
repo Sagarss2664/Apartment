e575d1f51e20cf5cdadfccc2b8686c03ff391391
SAGAR S SHEGUNASHI <130956680+Sagarss2664@users.noreply.github.com>
2025-03-16 18:40:21 +0530

    Update mserver.js

--- backend/mserver.js
+++ backend/mserver.js
@@ -1038,7 +1038,7 @@ app.get('/getBillLogs/:status', async (req, res) => {
                 date: bill.date || 'N/A',
                 time: bill.time || 'N/A',
                 utr_number: bill.utr_number || 'N/A',
-                amountToBePaid : bill.amountToBePaid|| 'N/A,
+                amountToBePaid : bill.amountToBePaid|| 'N/A',
             })),
         });
     } catch (err) {
